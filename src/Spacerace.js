e7c8b899f193584c28ef20852cc95a96305708bf
ilianac <idc2117@columbia.edu>
2019-04-03 20:04:48 -0400

    TEMP- DO NOT MERGE

--- src/Spacerace.js
+++ src/Spacerace.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -8,6 +7,8 @@ import ShowSpinner from './components/spinner';
 import Arcade from './fonts/Spacerace/title.TTF'; 
 import Avenir from './fonts/Spacerace/arcade.TTF'; 
 import './style/spacerace.css';
+import DifficultyTab from './DifficultyTab'; 
+import Button from 'react-button-component'
 import SpaceracePlay from './SpaceracePlay';
 
 import { 
@@ -19,19 +20,29 @@ import {
 
 import { getCurrentLessonForUser } from './actions/homepage';
 
+const CustomButton = Button.extend`
+        margin-top:4vh;
+        height: 82px;   
+        width: 270px;   
+        border: 5px solid #F5A623;  
+        border-radius: 10px;    
+        background-color: #FFFFFF;
+            font-size: 3.5rem;
+`
+
 class Spacerace extends Component {
   constructor(props) {
     super(props);
     this.state ={
-			headerLinks: ["Games", "Learn", "Home"],
-			isPlay: false,
-			difficulty: "easy"
-		}	
-	}
-	
-	play = () => {
-		this.setState({isPlay:true})
-	}
+            headerLinks: ["Games", "Learn", "Home"],
+            isPlay: false,
+            difficulty: "easy"
+        }   
+    }
+    
+    play = () => {
+        this.setState({isPlay:true})
+    }
 
   render() {
     const { 
@@ -44,88 +55,99 @@ class Spacerace extends Component {
 
     const { 
       headerLinks, 
-		} = this.state;
-		
-		return (
-			<div>
-				<Header 
-					links={headerLinks} 
-					isLoggedIn={this.props.isLoggedIn} 
-					username={this.props.currentUser.username}>
-				</Header>	
-				{this.state.isPlay === true ? (
-					<SpaceracePlay
-						difficulty={this.state.difficulty}
-						>
-					</SpaceracePlay>
-				) : (
-					<SpaceraceDescription
-						play={this.play}>
-					</SpaceraceDescription>
-				)}
-			</div>
-		)
-	}
+        } = this.state;
+        
+        return (
+            <div>
+                <Header 
+                    links={headerLinks} 
+                    isLoggedIn={this.props.isLoggedIn} 
+                    username={this.props.currentUser.username}>
+                </Header>   
+                {this.state.isPlay === true ? (
+                    <SpaceracePlay
+                        difficulty={this.state.difficulty}
+                        >
+                    </SpaceracePlay>
+                ) : (
+                    <SpaceraceDescription
+                        play={this.play}>
+                    </SpaceraceDescription>
+                )}
+            </div>
+        )
+    }
 }
 
 const SpaceraceDescription = ({ play }) => {
 
-	return (
-		<div className="MetaWrapper">
+    return (
+        <div className="MetaWrapper">
 
-			<div className = "GameTitleContainer">
-				
-				<div className="GameTitle">
-					<p>Space Race</p>
-				 </div>
+            <div className = "GameTitleContainer">
+                
+                <div className="GameTitle">
+                    <p>Space Race</p>
+                 </div>
 
-			</div>
+            </div>
 
-			<div className = "TextBoxContainer">
+            <div className = "TextBoxContainer">
 
-				<div className="TextBox">
+                <div className="TextBox">
 
-					<p>Type the words on the asteroids as they appear to eliminate them 
-							before they make impact on Earth. Each time an asteroid makes it through, 
-							you will lose a life. You start with three lives. You can gain a life each time 
-							you make it to the next level. As the levels increase, the number of asteroids also 
-							increase in number.
-					</p>
+                    <p>Type the words on the asteroids as they appear to eliminate them before they make impact on Earth. Each time an asteroid makes it through, you will lose a life. You start with three lives. You can gain a life each time you make it to the next level. As the levels increase, the number of asteroids also increase in number.
+                    </p>
 
-				</div>
+                </div>
 
-			</div>
+            </div>
 
-			<div className = "PlayButton">
+            <div className = "DifficultyTab">
+                <div className= "DifficultyText">
+                    <p> SELECT DIFFICULTY </p>
 
-				<button onClick = {play}> Play </button>
-	     
-			 </div>
+                </div>
 
-		 </div>					
-	)
+                <DifficultyTab difficulty={this.EzSelected} updateMed={this.MedSelected} updateHard={this.HardSelected}/>
+                <button onClick = {play}> Play </button>
+         
+             </div>
+        	
+			<div className = "PlaySection">
+            <div className = "PlayBackground">
+                <div className = "PlayStars">
+                    <img src="../public/images/spacerace/StarsBackground.svg" alt=""/>
+                </div>
+            </div>
+        </div>
+
+         </div>                 
+    )
 }
 
+
+
 const mapDispatchToProps = dispatch => {
-	return bindActionCreators({ 
-		fetchAllChapterNames, 
-		fetchAllPairs, 
-		fetchCompletedLessons,
-		fetchLessonById,
-		getCurrentLessonForUser
-	}, dispatch);
+    return bindActionCreators({ 
+        fetchAllChapterNames, 
+        fetchAllPairs, 
+        fetchCompletedLessons,
+        fetchLessonById,
+        getCurrentLessonForUser
+    }, dispatch);
 }
 
 const mapStateToProps = ({ app, auth }) => {
-	return {
-		allChapters: app.allChapters,
-		isLoading: app.isLoading,
-		chapterLessonPairs: app.chapterLessonPairs,
-		completedLessons: app.completedLessons,
-		currentUser: auth.currentUser,
-		isLoggedIn: auth.isLoggedIn,
-		currentLessonName: app.currentLesson.lessonName
-	}
+    return {
+        allChapters: app.allChapters,
+        isLoading: app.isLoading,
+        chapterLessonPairs: app.chapterLessonPairs,
+        completedLessons: app.completedLessons,
+        currentUser: auth.currentUser,
+        isLoggedIn: auth.isLoggedIn,
+        currentLessonName: app.currentLesson.lessonName
+    }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Spacerace); 
--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { withRouter } from 'react-router'
 
 import { Route, Switch, Redirect } from 'react-router-dom'
 
+
 import LoginPage from './LoginPage';
 import SignupPage from './SignupPage';
 import Profile from './ProfilePage';
@@ -11,6 +12,12 @@ import Learn from './Learn';
 import Tutorial from './Tutorial';                               
 import HomePage from './HomePage';
 import FourOhFour from './components/FourOhFour';
+import Challenge from './Challenge'
+import CocoType from './CocoType';
+import KeyTracker from './KeyTracking'
+import Stats from './Statistics'
+import Spacerace from './Spacerace'
+
 
 class App extends Component {
   constructor(props) {
@@ -59,6 +66,10 @@ class App extends Component {
       <Switch>
         <Route exact path="/" component={() => <LoginPage onSuccessfulAuth={this.onSuccessfulAuth}/>}/>
         <Route path="/signup" component={SignupPage}/>
+        <Route path="/spacerace" component={Spacerace}/>
+        <Route path="/challenge" component={Challenge}/>
+        <Route path="/coco" component={KeyTracker}/>
+        <Route path="/finalstats" component={Stats}/>
         <Route component={FourOhFour} />
       </Switch>
     )
